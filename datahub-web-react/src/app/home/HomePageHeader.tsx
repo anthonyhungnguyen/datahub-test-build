9408817038c850c8fe7c4495c6736f3329951d8e
hungnp <hungnp1@ghn.vn>
2024-07-31 22:32:17 -0400

    rebuild

--- datahub-web-react/src/app/home/HomePageHeader.tsx
+++ datahub-web-react/src/app/home/HomePageHeader.tsx
@@ -47,7 +47,7 @@ const styles = {
     searchContainer: { width: '100%', marginTop: '40px' },
     logoImage: { width: 400 },
     searchBox: { width: '50vw', minWidth: 400, margin: '40px 0px', marginBottom: '12px', maxWidth: '650px' },
-    subtitle: { marginTop: '28px', color: '#FFFFFF', fontSize: 12 },
+    subtitle: { marginTop: '28px', color: '#808080', fontSize: 12 },
 };
 
 const HeaderContainer = styled.div`
